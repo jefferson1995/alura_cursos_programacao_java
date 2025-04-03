eb6670041f3a5d551353481563909266a05b2204
Jefferson Barbosa <jefferson.bds@hotmail.com>
2025-04-02 21:58:12 -0300

    Deletando projeto, inserir apenas o final ao terminar o curso

--- Java_e_Spring_Security_crie_perfis_e_autorize_requisicoes/3866-seguranca-java-2-aula-03/src/main/resources/db/migration/V5__alter-table-consultas.sql
+++
@@ -1,5 +0,0 @@
-alter table consultas drop column paciente;
-
-alter table consultas
-add column paciente_id bigint not null,
-add constraint fk_consultas_paciente_id foreign key(paciente_id) references pacientes(id);
